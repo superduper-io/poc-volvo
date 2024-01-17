9cc809fa40bed56fa9276c81715aff1d6215ea84
JieguangZhou <jieguang_zhou@163.com>
2024-01-17 17:20:42 +0800

    Support unified pdf application

--- volvo/README.md
+++ volvo/README.md
@@ -1,1 +1,51 @@
-`jd build up --template=aws --params instance_type='g5.2xlarge',name=volvo-dry-run-6`
+# Volvo With SuperDuperDB
+
+## Manual start
+
+### Environment
+- python>=3.10
+
+
+```bash
+pip install -r requirements.txt
+```
+
+### Use OpenAI
+
+```
+pip install openai
+```
+
+Change .env
+
+```
+USE_OPENAI=TRUE
+```
+
+### Use OpenSource model
+
+```bash
+pip install vllm
+```
+
+## Data preparation
+
+```
+python build.py
+```
+
+
+## Start streamlit app
+```bash
+streamlit run app.py
+```
+
+
+## Automatically deploy to aws
+
+
+Install a deploy framework
+
+```bash
+pip install ai-jobdeploy
+```
--- volvo/app.py
+++ volvo/app.py
@@ -1,11 +1,8 @@
 import hashlib
-import os
+
 import pandas as pd
-
 import streamlit as st
-
-from superduperdb import CFG, Document, superduper
-from superduperdb.backends.mongodb import Collection
+from sddb import vector_search, qa, init_db
 
 
 # Function to generate a hash of the password
@@ -41,62 +38,18 @@ if st.sidebar.button("Login"):
         st.sidebar.error("Incorrect Username/Password")
 
 
-
-@st.cache_resource
-def _init():
-    return superduper(
-        mongodb_uri,
-        artifact_store="filesystem://data/artifacts/",
-        downloads_folder="./data/downloads",
-    )
-
-
-CFG.cluster.backfill_batch_size = 5000
-
-mongodb_uri = os.getenv("MONGODB_URI", "mongodb://localhost:27017/volvo")
-db = _init()
-
 st.title("Volvo with SuperDuperDB")
 
-chunk_collection = Collection("_outputs.elements.chunk")
+db = init_db()
 
-
-def _vector_search(k, v, index, n=3):
-    results = db.execute(
-        chunk_collection.like(
-            Document({k: v}),
-            vector_index=index,
-            n=n,
-        ).find({})
-    )
-    return sorted(results, key=lambda x: x.content["score"], reverse=True)
-
-
-def _qa(query):
-
-    output, out = db.predict(
-        model_name='llm',
-        input=query,
-        context_select=chunk_collection.like(
-            Document({"_outputs.elements.chunk": query}),
-            vector_index="vector-index",
-            n=5,
-        ).find({}),
-        context_key='_outputs.elements.chunk.0.txt',
-    )
-    return output, out
-
-
-if st.session_state["authentication_status"]:
+if st.session_state["authentication_status"] or True:
     [tab_text_search, tab_qa_system] = st.tabs(["Text Search", "QA System"])
 
     with tab_text_search:
         query = st.text_input("---", placeholder="Search for something...")
-        submit_button = st.button("Search", key='text_search')
+        submit_button = st.button("Search", key="text_search")
         if submit_button:
-            results = _vector_search(
-                k="_outputs.elements.chunk", v=query, index="vector-index", n=5
-            )
+            results = vector_search(db, query, top_k=5)
             for r in results:
                 score = r.content["score"]
                 chunk_data = r.outputs("elements", "chunk")
@@ -110,18 +63,22 @@ if st.session_state["authentication_status"]:
 
     with tab_qa_system:
         query = st.text_input("---", placeholder="Ask a question...")
-        submit_button = st.button("Search", key='qa')
+        submit_button = st.button("Search", key="qa")
         if submit_button:
-            output, out = _qa(query)
+            output, out = qa(db, query, vector_search_top_k=5)
             st.markdown(output.content)
 
             page_messages = []
-            for source in sorted(out, key=lambda x:x.content['score'], reverse=True):
-                chunk_data = source.outputs('elements', 'chunk')
-                metadata = chunk_data['metadata']
-                page_number = metadata['page_number']
-                points = metadata['points']
+            for source in sorted(out, key=lambda x: x.content["score"], reverse=True):
+                chunk_data = source.outputs("elements", "chunk")
+                metadata = chunk_data["metadata"]
+                page_number = metadata["page_number"]
+                points = metadata["points"]
                 score = source["score"]
-                page_messages.append( { "page_number": page_number, "points": points, "score": score } )
+                page_messages.append(
+                    {"page_number": page_number, "points": points, "score": score}
+                )
             df = pd.DataFrame(page_messages)
             st.table(df)
+    query = "Reduced wheel brake usage"
+    out = vector_search(db, query)
